ef2e7591eb6fc7408041189a22591d8264e799df
osomige <osomige725@gmail.com>
2020-10-27 11:45:56 +0900

    最終修正

--- server/resources/views/article/create.blade.php
+++ server/resources/views/article/create.blade.php
@@ -14,8 +14,9 @@
             <input type="string" name="title" value="{{ old('title')}}">
             {{-- コラムネームでバリューも --}}
         </p>
-        <p>本文<br>
-        <textarea type="text" name="body" value="{{ old('body') }}"></textarea>
+        <p>
+            本文<br>
+            <textarea type="text" name="body" value="{{ old('body') }}"></textarea>
         </p>
         <input type="submit" value="投稿">
     </form>
